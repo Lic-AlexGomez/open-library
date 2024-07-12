1bc677394bc613ea1738155d89351902f8c35881
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-07-12 16:46:25 -0400

    update

---
+++ precache-manifest.df1ed4b9a785c02c71adae4dd6764e45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8879ad9baff4a8ab17c8aea7c4127dd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b3509e18e11897c224b",
+    "url": "/static/js/2.5cdc3a55.chunk.js"
+  },
+  {
+    "revision": "7ceeddd0962a37302cc3",
+    "url": "/static/js/3.4cf3234c.chunk.js"
+  },
+  {
+    "revision": "c8d3674ee61db924fd85",
+    "url": "/static/js/main.403bd330.chunk.js"
+  },
+  {
+    "revision": "d3f218b9a34b403fe639",
+    "url": "/static/js/runtime~main.9845b581.js"
+  }
+]);
